5fee95e29eb05de7e3ae1dbefa6420e94455cb27
reneventuraamaral <profrene@gmail.com>
2025-01-13 20:24:37 -0300

    Correção no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
 
 export default nextConfig;
@@ -13,3 +14,4 @@ module.exports = {
     },
   },
 };
+module.exports = nextConfig;
