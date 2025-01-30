231eac0f61520122880de23cba32efcabdcb9e3f
reneventuraamaral <profrene@gmail.com>
2025-01-29 23:24:15 -0300

    Primeiro commit - Nova versão do projeto

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
 };
 
 export default nextConfig;
-
-module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true, // Ignora erros do ESLint durante o build
-  },
-  api: {
-    bodyParser: {
-      sizeLimit: '1mb', // Ajuste conforme necessário
-    },
-  },
-};
-module.exports = nextConfig;
